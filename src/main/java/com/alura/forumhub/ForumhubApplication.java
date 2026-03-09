c0c56dc1e1ea4ba722971bc5e0f422d6f2f40b20
ThallesHR <thalles.rosa@sou.unifal-mg.edu.br>
2026-03-09 15:35:12 -0300

    new fix

--- src/main/java/com/alura/forumhub/ForumhubApplication.java
+++ src/main/java/com/alura/forumhub/ForumhubApplication.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class ForumhubApplication {
 
 	public static void main(String[] args) {
+		// isso serve para que você como adm tester pegue o token mas caso produção retire isso!!
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		System.out.println(encoder.encode("123456"));
 		SpringApplication.run(ForumhubApplication.class, args);
